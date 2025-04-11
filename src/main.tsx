f0dd4106cb97828115b1a22b72fa93558a725ece
slaw469 <stelaw469@gmail.com>
2025-04-11 10:04:13 -0500

    about updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import './index.css'
 import './App.css'
 
 // Import CSS files
