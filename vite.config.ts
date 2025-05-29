293a50a12a29418d0e233e5e3cb6fa8b1d043df3
slaw469 <stelaw469@gmail.com>
2025-05-29 18:47:40 -0500

    link removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'tanstack-router': ['@tanstack/react-router']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
